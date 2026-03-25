1157892bdd16d0491a222d8a19ad599740c8530d
sathvika6791-star <sathvika6791@gmail.com>
2026-03-25 11:38:44 +0530

    exp7

--- sc1.js
+++ sc1.js
@@ -1,11 +1,10 @@
-//object destructuring
-const person = {
-    name: "Alia",
-    age: 31,
-    city: "Mumbai"
-};
-const { name, age, city } = person;
+class Student {
+    constructor(name, rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
 
-console.log(name);
-console.log(age);
-console.log(city);
+    }
+    display() {
+        return this.name + "-" + this.rollNo;
+    }
+}
